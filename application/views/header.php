5a7b032f3d8da273861fa93ffad6b316c25c40d8
JordiB <bravo_94@hotmail.es>
2016-02-24 21:07:49 +0100

    Vista inicial creada

---
+++ application/views/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        
+    </head>
+    <body>
