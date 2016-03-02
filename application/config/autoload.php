953a9d3f4b64cf77df6a3a44f8541181082a8b77
abenitsi <albertbenitez92@gmail.com>
2016-03-02 19:25:53 +0100

    Afegit Controladors

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -126,4 +126,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Empleado');
+$autoload['model'] = array('Empleado', 'Departaments');
